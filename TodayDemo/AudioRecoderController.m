8073f0f861d1fcd2bd9d25c164440d6074c9d230
jalon <jalon.wang@refinedchina.com>
2019-12-06 16:39:26 +0800

    audio

--- TodayDemo/AudioRecoderController.m
+++ TodayDemo/AudioRecoderController.m
@@ -10,11 +10,16 @@
 #import <AVFoundation/AVFoundation.h>
 #import "UIViewController+WH.h"
 
+
 @interface AudioRecoderController ()<AVAudioRecorderDelegate,UITableViewDelegate,UITableViewDataSource>
 @property (weak, nonatomic) IBOutlet UITableView *tabV;
 @property (strong, nonatomic) AVAudioRecorder *recorder;
+@property (strong, nonatomic) AVAudioPlayer *player;
 @property (weak, nonatomic) IBOutlet UILabel *timeLab;
 @property (strong,nonatomic)NSTimer *timer;
+@property (copy,nonatomic)NSArray *contentsArr;
+@property (weak, nonatomic) IBOutlet UILabel *dbLab;
+
 @end
 
 @implementation AudioRecoderController
@@ -29,7 +34,7 @@
     if (![session setActive:YES error:&error]) {
         NSLog(@"setActive error==%@",error.localizedDescription);
     }
-    
+     [self upDateTab];
 }
 
 - (AVAudioRecorder *)recorder{
@@ -48,6 +53,8 @@
         NSError*error;
         _recorder = [[AVAudioRecorder alloc] initWithURL:url settings:settings error:&error];
         if (_recorder) {
+            //测量录音分贝
+            _recorder.meteringEnabled = YES;
             _recorder.delegate = self;
             [_recorder prepareToRecord];
         }else{
@@ -57,7 +64,14 @@
     return _recorder;
 }
 
-
+- (void)measureDb{
+    [self.recorder updateMeters];
+    //-160db~0db
+    CGFloat averageDb = [self.recorder averagePowerForChannel:0];
+    CGFloat peakDb = [self.recorder peakPowerForChannel:0];
+    self.dbLab.text = [NSString stringWithFormat:@"平均:%f--峰值:%f",averageDb,peakDb];
+    
+}
 
 
 
@@ -70,6 +84,7 @@
         if (![self.recorder record]) {
             NSLog(@"start recorder fail !!!  ");
         }
+          
             [self beginTime];
     }else{
         [self.recorder pause];
@@ -97,16 +112,38 @@
 }
 #pragma mark -tab
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return 0;
+    return self.contentsArr.count;
 }
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    return nil;
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"cell"];
+    }
+    NSURL *url = self.contentsArr[indexPath.row];
+    NSString*name = url.lastPathComponent;
+    
+    NSArray*separators = [name componentsSeparatedByString:@"-"];
+    NSString *time = separators.lastObject;
+    NSTimeInterval stamp =    [time doubleValue];
+    NSDate *date = [NSDate dateWithTimeIntervalSinceReferenceDate:stamp];
+    NSDateFormatter *format = [[NSDateFormatter alloc] init];
+    format.dateFormat = @"yyyy-MM-dd HH:mm:ss";
+    cell.textLabel.text= separators[0];
+    cell.detailTextLabel.text = [format stringFromDate:date];
+    return cell;
+    
 }
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    [self playAudio:indexPath];
+}
+
 
 - (void)saveAudio:(NSString*)name{
     NSString *home = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
     NSTimeInterval timestamp = [NSDate timeIntervalSinceReferenceDate];
-    NSString *saveName = [NSString stringWithFormat:@"%@-%f",name,timestamp];
+    NSString *saveName = [NSString stringWithFormat:@"%@-%f.caf",name,timestamp];
     NSString *savePath = [home stringByAppendingPathComponent:saveName];
     NSURL *tmpUrl = self.recorder.url;
     if (tmpUrl) {
@@ -132,6 +169,8 @@
         int minute = duration/60%60;
         int hour = duration/3600;
         weakself.timeLab.text = [NSString stringWithFormat:@"%02d:%02d:%02d",hour,minute,second];
+        //测量db
+        [weakself measureDb];
     }];
 }
 
@@ -139,11 +178,43 @@
     NSFileManager * manager = [NSFileManager defaultManager];
    
     NSURL*docUrl = [[manager URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
-    NSArray*urls =  nil;
-    for (NSURL*url in urls) {
-        NSLog(@"path=======%@",url.absoluteString);
+    NSError *error;
+//    NSArray*fileNames = [manager contentsOfDirectoryAtPath:docUrl.path error:&error];
+//    if (!error) {
+//        for (NSString * name in fileNames) {
+//            NSLog(@"file name----%@",name);
+//        }
+//    }else{
+//        NSLog(@"get files error---%@",error.localizedDescription);
+//    }
+    NSArray *urls = [manager contentsOfDirectoryAtURL:docUrl includingPropertiesForKeys:[NSArray array] options:0 error:&error];
+    if (!error) {
+//        for (NSURL * u in urls) {
+//            NSLog(@"url path ----%@",u.path);
+//        }
+    }else{
+        NSLog(@"get urls error---%@",error.localizedDescription);
     }
+
+    self.contentsArr =     [urls sortedArrayUsingComparator:^NSComparisonResult(NSURL*  _Nonnull obj1, NSURL*  _Nonnull obj2) {
+        double   o1 = [[[obj1.lastPathComponent componentsSeparatedByString:@"-"] lastObject] doubleValue];
+        double  o2 = [[[obj2.lastPathComponent componentsSeparatedByString:@"-"] lastObject] doubleValue];
+        return o1<o2;
+    }];;
+    [self.tabV reloadData];
 }
 
-
+- (void)playAudio:(NSIndexPath* )indexPath{
+    [self.player stop];
+    
+    NSURL *url = self.contentsArr[indexPath.row];
+    NSError *error = nil;
+    self.player = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:&error];
+    if (error) {
+        NSLog(@"player fail ---%@",error.localizedDescription);
+        return;
+    }
+    [self.player play];
+    
+}
 @end
